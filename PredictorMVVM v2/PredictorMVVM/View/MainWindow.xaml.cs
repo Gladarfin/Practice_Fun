d023c80eea450d1b2e629575960cae7a4fe0bbc8
GladarfinVS <gladarfinvs@gmail.com>
2021-04-25 19:51:19 +0500

    Убрал Converter из первой версии, добавлен отдельный проект v2

---
+++ PredictorMVVM v2/PredictorMVVM/View/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PredictorMVVM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Сломал все MVVM, не, ну а вы видели какую простыню надо написать, чтобы Drag&Drop сделать?
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+    }
+}
---
+++ PredictorMVVM v2/PredictorMVVM/ViewModel/MainWindowVM.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace PredictorMVVM.ViewModel
+{
+    public class MainWindowVM : BaseVM
+    {
+        public int ProgressBarValue { get; set; }
+
+        public string TextBlockValue { get; set; }
+
+        public ICommand ButtonClick { get; set; }
+
+        public ICommand CloseApp { get; set; }
+
+        public string Prediction { get; set; }
+
+        public double OpacityTB { get; set; } = 0;
+
+        private string PredictionsFromFile { get; set; }
+
+        private string[] predictions;
+
+        private bool IsLoaded { get; set; }
+        private readonly Random randomNumber = new Random();
+
+        public MainWindowVM()
+        {
+            ButtonClick = new RelayCommand(Click);
+            CloseApp = new RelayCommand(Close);
+            WindowLoaded();         
+        }
+
+        private void Close()
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void WindowLoaded()
+        {
+            PredictionsFromFile = File.ReadAllText(Environment.CurrentDirectory + Properties.Settings.Default.PredictionsPath);
+        }
+
+        private async void Click()
+        {
+
+            await Task.Run(() =>
+            {
+                Prediction = "";
+                OpacityTB = 0;
+                Thread.Sleep(500);
+            });
+
+            IsLoaded = false;
+
+            if (PredictionsFromFile.Length == 0)
+            {
+                MessageBox.Show("Шар сломан :(", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            else if (!IsLoaded)
+            {
+                predictions = JsonConvert.DeserializeObject<string[]>(PredictionsFromFile);                
+                IsLoaded = true;
+            }
+                var index = randomNumber.Next(predictions.Length);
+                Prediction = predictions[index];
+        }
+    }
+}
